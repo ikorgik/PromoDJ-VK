8194a4f931a2694cbd257a26f441b2af5e571804
ikorgik <ikorgik@gmail.com>
2016-10-07 02:30:25 +0300

    Fixed title encode issues.

--- src/main.js
+++ src/main.js
@@ -1,42 +1,37 @@
 
-var items = document.querySelectorAll('.track2 .title');
-
-Array.prototype.forEach.call(items, function (item) {
-  var title = item.querySelector('a.invert').innerHTML.replace('&amp;', '%26').replace('&nbsp;', ' ');
-  console.log(title);
- // Sia – The Greatest (Amice Remix)
-  // Sia&nbsp;–&nbsp;The Greatest (Amice Remix)
-
-  var vkLink = document.createElement('a');
-  vkLink.appendChild(document.createTextNode("[vk]"));
-  vkLink.title = title;
-  vkLink.target = "_blank";
-  vkLink.href = "https://vk.com/search?c[q]=" + title + "&c[section]=audio";
-  item.appendChild(vkLink);
-
-  var ymLink = document.createElement('a');
-  ymLink.appendChild(document.createTextNode("[ym]"));
-  ymLink.title = title;
-  ymLink.target = "_blank";
-  ymLink.href = "https://music.yandex.ru/search?text=" + title;
-
-  item.appendChild(ymLink);
-});
 // Alternative of DOMContentLoaded (doesn't work somewhy).
 document.onreadystatechange = function () {
   if (document.readyState == "complete") {
-    var items = document.querySelectorAll('.trendy div.playerr_title');
 
-    Array.prototype.forEach.call(items, function (item) {
-      //var title = item.querySelector('a').innerHTML.replace('&nbsp;', " ").replace('&amp;', '%26');
-      var title = item.querySelector('a').innerHTML;
-      title = title.replaceHtmlEntites();
+    var listItems = document.querySelectorAll('.track2 .title');
+
+    Array.prototype.forEach.call(listItems, function (item) {
+      var title = item.querySelector('a').innerHTML.replaceHtmlEntites();
       title = encodeURI(title);
-      //title = title.replace(/&/g, '%26').replace(/#/g, '%23');
+      title = title.replace(/&/g, '%26').replace(/#/g, '%23');
 
-      console.log(title);
+      var vkLink = document.createElement('a');
+      vkLink.appendChild(document.createTextNode("[vk]"));
+      vkLink.title = title;
+      vkLink.target = "_blank";
+      vkLink.href = "https://vk.com/search?c[q]=" + title + "&c[section]=audio";
+      item.appendChild(vkLink);
 
+      var ymLink = document.createElement('a');
+      ymLink.appendChild(document.createTextNode("[ym]"));
+      ymLink.title = title;
+      ymLink.target = "_blank";
+      ymLink.href = "https://music.yandex.ru/search?text=" + title;
 
+      item.appendChild(ymLink);
+    });
+
+    var trendyItems = document.querySelectorAll('.trendy div.playerr_title');
+
+    Array.prototype.forEach.call(trendyItems, function (item) {
+      var title = item.querySelector('a').innerHTML.replaceHtmlEntites();
+      title = encodeURI(title);
+      title = title.replace(/&/g, '%26').replace(/#/g, '%23');
 
       var vkLink = document.createElement('a');
       vkLink.appendChild(document.createTextNode("[vk]"));
@@ -59,10 +54,9 @@ document.onreadystatechange = function () {
 
 String.prototype.replaceHtmlEntites = function() {
   var s = this;
-  var translate_re = /&(nbsp|amp|quot|lt|gt);|#/g;
-  var translate = {"nbsp": " ", "amp" : "%26", "quot": "\"","lt"  : "<","gt"  : ">", "#": "%23"};
+  var translate_re = /&(nbsp|amp|quot|lt|gt);/g;
+  var translate = {"nbsp": " ", "amp" : "&", "quot": "\"","lt"  : "<","gt"  : ">"};
   return ( s.replace(translate_re, function(match, entity) {
     return translate[entity];
   }) );
 };
-
